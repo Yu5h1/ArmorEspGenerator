0c8e79e1308f280e55626696ab0ba8d916065845
Yu5h1 <JenYu5h10415@gmail.com>
2020-09-27 11:36:00 +0800

    start

--- ArmorEspGenerator/NifUtil.cs
+++ ArmorEspGenerator/NifUtil.cs
@@ -5,15 +5,15 @@ namespace TESV_EspEquipmentGenerator
 {
     public static class NifUtil
     {
-        [DllImport("ousniusNifLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("NifUtility.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern string GetShapesName([MarshalAs(UnmanagedType.LPStr)]string filename);
         public static string[] GetShapesNames(string filename) => GetShapesName(filename).GetLines();
-        [DllImport("ousniusNifLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("NifUtility.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern string GetShapeTextures([MarshalAs(UnmanagedType.LPStr)]string filename, [MarshalAs(UnmanagedType.LPStr)]string shapename);
         public static string[] GetShapeTexturesArray(string filename, string shapename)
                                                         => GetShapeTextures(filename, shapename).GetLines();
 
-        [DllImport("ousniusNifLib.dll", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport("NifUtility.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern string GetBSDismemberBodyParts([MarshalAs(UnmanagedType.LPStr)]string filename);
 
 
